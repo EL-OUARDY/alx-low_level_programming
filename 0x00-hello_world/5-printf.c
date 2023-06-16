513b29cf117a0f72b4df4f3f2d9cb9636be7db8a
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-15 19:24:41 -0700

    5-printf.c task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - the entry point of the program
+* @void: we do not have any args
+*
+* Description: all program logic goes inside main function
+* our program is simple and for education purpose only
+* Return: nothing! - 0 as a retcode
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
