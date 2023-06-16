4871f986a2bf9dba5c9936a020264d9cbe9554e0
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-15 18:51:07 -0700

    add description comment for main()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+*This the main function
+*The entry point of my program
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
