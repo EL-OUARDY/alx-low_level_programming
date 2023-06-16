03c8145d5fb81c1407f4ad13c7e2d18a8622263b
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-15 19:00:00 -0700

    add comment for puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	/* display a message on screen */
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
