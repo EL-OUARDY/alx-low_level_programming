93e2ab54ec70fbb5054d628229f1884f349928f6
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-20 19:52:20 -0700

    10-add.c task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - calculate the sum of two intergers
+ * @a: interger
+ * @b: interger
+ * Return: int - the sum of the two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
