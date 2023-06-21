6bcc0d2d25c7cc85dea6c175b819cc2db62db653
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-20 18:16:24 -0700

    print_sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - check for a number sign
+ * @n: integer is passed as an argument
+ * Description: receive a and act based on his sign
+ * Return: int
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
