3a635ddacc73817daa836e982f7e7474b05d398b
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-22 17:19:30 -0700

    2-mul.c task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - basic multiplication
+ * @a: integer is passed as an argument
+ * @b: integer is passed as an argument
+ * Description: receive two interger and do math multiplication
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
