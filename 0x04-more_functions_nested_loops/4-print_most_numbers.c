40284c55765a12fcefd7e48f36cc9d643b212455
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-22 17:38:43 -0700

    4-print_most_numbers.c task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers from 0 to 9 except 2 and 4
+* @void: no argument passed
+* return: nothing
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
