e4b92d781db722c229abd75399beeee4a03a5d02
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-27 19:33:12 -0700

    4-print_rev.c task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - prints a given string in reverse
+* @s: a pointer to a char passed as an argument
+* Return: Nothing
+*/
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (*s++)
+		l++;
+
+	s-=2;
+	while (l > 0)
+	{
+		_putchar(*s);
+		l--;
+		s--;
+	}
+	_putchar('\n');
+}
