b7ab5d4b780bf7a0d8ee58a215bbd23d494d491e
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-27 18:31:27 -0700

    betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* more_numbers - receive a pointer to an int then update its value
+* reset_to_98 - receive a pointer to an int then update its value
 * @*n: a pointer passed as an argument
 * return: nothing
 */
