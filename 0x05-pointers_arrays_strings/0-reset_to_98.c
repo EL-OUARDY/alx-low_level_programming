50fc63fef55d1b694d6161be0faa1989df8e39df
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-27 18:33:01 -0700

    betty style for variable n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
 * reset_to_98 - receive a pointer to an int then update its value
-* @*n: a pointer passed as an argument
+* @n: a pointer passed as an argument
 * return: nothing
 */
 void reset_to_98(int *n)
