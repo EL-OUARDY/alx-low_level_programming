29b4e0432e2106ac2293cc1de1919461609ba92c
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-30 10:54:41 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *cap_string - Capitalize words
+ * @str: input string
+ * Return: a pointer to the result string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+			|| (str[i - 1] == ' ')
+			|| (str[i - 1] == '\t')
+			|| (str[i - 1] == '\n')
+			|| (str[i - 1] == ',')
+			|| (str[i - 1] == ';')
+			|| (str[i - 1] == '.')
+			|| (str[i - 1] == '!')
+			|| (str[i - 1] == '?')
+			|| (str[i - 1] == '"')
+			|| (str[i - 1] == '(')
+			|| (str[i - 1] == ')')
+			|| (str[i - 1] == '{')
+			|| (str[i - 1] == '}')
+				str[i] -= 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
