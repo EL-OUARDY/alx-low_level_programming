f0fc87a8ed32e0a8b9f9b70a7f3265a723d192a4
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-06-30 08:58:45 -0700

    3-strcmp.c TASK

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compare two strings
+ * @s1: string 1
+ * @s2: atring 2
+ * Return: a an integer indicates the difference between s1 & s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
