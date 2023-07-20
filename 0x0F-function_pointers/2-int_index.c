fd50bb11cc34af109609a8be60859951c14f57e1
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-07-20 16:42:35 -0700

    2-int_index.c TASK

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,15 +2,26 @@
 #include <stdlib.h>
 
 /**
- * print_name - print a given name
- * @name: string to add
- * @f: a pointer to a function
- * Description: Print given name using a passed pointer function
- * Return: nothing -_-
+ * int_index - Search function
+ * @array: input array
+ * @size: array size
+ * @cmp: a pointer to a function to be used to compare values
+ * Description: a function that searches for an integer.
+ * Return: index of element OR -1 if there is no matches (or size <= 0)
 */
-void print_name(char *name, void (*f)(char *))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+			return (i);
+	}
+	return (-1);
 }
