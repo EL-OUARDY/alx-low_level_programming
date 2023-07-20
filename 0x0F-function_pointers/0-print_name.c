09e7eb76dddb6d46781154265e19252ced0c0dda
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-07-20 15:33:50 -0700

    0-print_name.c TASK

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - print a given name
+ * @name: string to add
+ * @f: a pointer to a function
+ * Description: Print given name using a passed pointer function
+ * Return: nothing -_-
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
