2343bb2e1631a23df2f1867693d21789a43cdaa4
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-08-31 15:13:12 -0700

    recursion - task 5 - sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int sqrt_recursion_helper(int num, int guess);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number,
+ * using recursion.
+ * @n: number parameter
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_recursion_helper(n, 0));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number,
+ * using recursion.
+ * @num: number parameter
+ * @guess: number parameter
+ * Return: integer
+ */
+int sqrt_recursion_helper(int num, int guess)
+{
+	if (guess * guess > num)
+		return (-1);
+
+	/* guess is right */
+	if (guess * guess == num)
+		return (guess);
+
+	return (sqrt_recursion_helper(num, guess + 1));
+}
