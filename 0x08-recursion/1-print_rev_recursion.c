b1edaf7903a52e63f9179ee27ea8d50e62f200e2
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-08-31 04:25:12 -0700

    fix betty no description

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - Prints a string in reverse order, using recursion.
+ * _print_rev_recursion - Prints a string in reverse order, using recursion.
  * @s: string to be printed
  * Return: void
  */
