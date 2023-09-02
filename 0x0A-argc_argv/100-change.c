6a7e0aae6bf95f755aacfcaaaafbe7511e032ca7
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-09-01 18:04:00 -0700

    task 100 - change implimentation

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - change caculator
  * @argc: the counter of the passed arguments
@@ -8,13 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int num_coins, cents;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
@@ -22,18 +25,20 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int numCoins = 0;
-
-	for (int i = 0; i < 5; i++)
+	for (num_coins = 0; cents > 0; num_coins++)
 	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			numCoins++;
-		}
+		if (cents - 25 >= 0)
+			cents = cents - 25;
+		else if (cents - 10 >= 0)
+			cents = cents - 10;
+		else if (cents - 5 >= 0)
+			cents = cents - 5;
+		else if (cents - 2 >= 0)
+			cents = cents - 2;
+		else if (cents - 1 >= 0)
+			cents = cents - 1;
 	}
 
-	printf("%d\n", numCoins);
+	printf("%d\n", num_coins);
 	return (0);
 }
