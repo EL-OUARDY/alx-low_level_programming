52d6ba3d96b0b8c11650f21638cb79f919bc4ea7
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-09-25 12:47:17 -0700

    more singly linked lists - task 5 - free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: the head of the targeted list
+ * Description: free all list nodes from memory
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = *head;
+	listint_t *next_node;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+
+	*head = NULL;
+}
