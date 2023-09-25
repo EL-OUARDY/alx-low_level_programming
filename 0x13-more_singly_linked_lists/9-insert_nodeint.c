4c5f996809007bcec132187168cd362778495f7a
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-09-25 16:45:11 -0700

    more singly linked lists - task 9 - fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	/* list is empty  */
-	if (head == NULL || *head == NULL)
+	if (!head)
 		return (NULL);
 
 	/* insert at the beginning  */
