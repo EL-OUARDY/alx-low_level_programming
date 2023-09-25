413a5fcd0378f66daae7c24b11e72af729279cc6
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-09-25 12:49:26 -0700

    more singly linked lists - task 5 - FIX free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,6 +14,10 @@ void free_listint2(listint_t **head)
 	listint_t *current_node = *head;
 	listint_t *next_node;
 
+	/* check the head pointer */
+	if (head == NULL)
+		return;
+
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
