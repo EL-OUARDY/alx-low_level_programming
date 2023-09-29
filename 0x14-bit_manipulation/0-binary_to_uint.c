9775d6e32c4aafbda8042e7d8116c16cafbda64f
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-09-29 15:28:33 -0700

    Bit manipulation - Task 0 - binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+
+int _pow(int base, unsigned int exponent);
+
+/**
+ * binary_to_uint - convert a binary to decimal
+ * @b: binary number input as a string of 0 and 1
+ * Description: converts a binary number to an unsigned int
+ * Return: the decimal number or 0 if b is null or b is invalid
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+        unsigned int decimal = 0, length;
+
+        /* check string */
+        if (b == NULL)
+                return (0);
+
+        /* string length */
+        for (length = 0; b[length]; length++);
+
+        /* loop over binary digits */
+        while (*b)
+        {
+                if (*b == '1')
+                {
+                        decimal += _pow(2, length - 1);
+                }
+                else if (*b != '0')
+                        return (0);
+                b++;
+                length--;
+        }
+
+        return (decimal);
+}
+
+/**
+ * _pow - perform power
+ * @base: base number
+ * @exponent: exponent number
+ * Description: custom power function (exponentiation) using loops
+ * Return: integer
+ */
+int _pow(int base, unsigned int exponent)
+{
+        unsigned int i, result = 1;
+
+        if (exponent == 0)
+                return (1);
+
+        for (i = 0; i < exponent; i++)
+                result *= base;
+
+        return result;
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef BIT_HEADER
+#define BIT_HEADER
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
