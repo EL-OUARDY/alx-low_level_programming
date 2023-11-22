e4a222bfd2c3a2916cc441ec7e59647d724d66b8
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-22 09:33:36 -0800

    Doubly linked lists — task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: the head of the targeted list
+ * Description: frees the memory located by the dlistint_t list
+ * Return: nothing, silence
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL) {
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
