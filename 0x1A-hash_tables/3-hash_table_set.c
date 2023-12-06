9abdfde8d7bc8241dd5e661a3743b5f29f4c7a22
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-12-06 09:03:14 -0800

    Hash tables — FIX: update iterator node instead of head node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(temp->key, key) == 0) /* match */
 			{
-				ht->array[index]->value = strdup(value); /* update value of node */
+				temp->value = strdup(value); /* update the value of the existed node */
 				free(new_node->key);
 				free(new_node->value);
 				free(new_node);
