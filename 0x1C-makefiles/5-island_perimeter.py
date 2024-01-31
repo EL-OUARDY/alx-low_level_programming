a8e0caec43bbe130e1878a1701033d07f0189c07
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-01-30 16:34:30 -0800

    Makefiles — task 5 - fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,17 +11,17 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:  # means cell is a land
                 # calculate edges (water neighbors)
-                # left cell
+                # # left cell
                 if grid[i][j - 1] == 0 or j == 0:
                     edges += 1
                 # right cell
-                if grid[i][j + 1] == 0 or j == (len(grid[i]) - 1):
+                if j == (len(grid[i]) - 1) or grid[i][j + 1] == 0:
                     edges += 1
                 # top cell
                 if grid[i - 1][j] == 0 or i == 0:
                     edges += 1
                 # bottom cell
-                if grid[i + 1][j] == 0 or i == (len(grid) - 1):
+                if i == (len(grid) - 1) or grid[i + 1][j] == 0:
                     edges += 1
 
     return edges
