9045a9ce428ccf4126f712a342e60cf3a9033123
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-17 06:58:59 -0700

    Fix betty style

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -9,52 +9,44 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *temp = NULL, *stop = NULL;
+	skiplist_t *temp = NULL, *stop = NULL;
 
-    /* Check if the list is valid */
-    if (!list)
-        return (NULL);
-
-    /* Initialize temp pointer */
-    temp = list;
-
-    /* Search for the value using the express lane */
-    while (temp && temp->express && temp->express->n < value)
-    {
-        printf("Value checked at index [%lu] = [%i]\n",
-               temp->express->index, temp->express->n);
-        temp = temp->express;
-    }
-
-    /* Determine the stop node */
-    stop = temp;
-    while (stop && stop->next != stop->express)
-        stop = stop->next;
-
-    /* Print information about the value if found */
-    if (temp->express)
-    {
-        printf("Value checked at index [%lu] = [%i]\n",
-               temp->express->index, temp->express->n);
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               temp->index, temp->express->index);
-    }
-    else
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               temp->index, stop->index);
-
-    /* Search for the value in the range */
-    while (temp != stop && temp->n < value)
-    {
-        printf("Value checked at index [%lu] = [%i]\n",
-               temp->index, temp->n);
-        temp = temp->next;
-    }
-    printf("Value checked at index [%lu] = [%i]\n",
-           temp->index, temp->n);
-
-    /* Check if the value is found and return the appropriate pointer */
-    if (temp == stop)
-        return (NULL);
-    return (temp);
+	if (!list)
+		return (NULL);
+	temp = list;
+	/* Search for the value using the express lane */
+	while (temp && temp->express && temp->express->n < value)
+	{
+		printf("Value checked at index [%lu] = [%i]\n",
+				temp->express->index, temp->express->n);
+		temp = temp->express;
+	}
+	/* Determine the stop node */
+	stop = temp;
+	while (stop && stop->next != stop->express)
+		stop = stop->next;
+	/* Print information about the value if found */
+	if (temp->express)
+	{
+		printf("Value checked at index [%lu] = [%i]\n",
+				temp->express->index, temp->express->n);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				temp->index, temp->express->index);
+	}
+	else
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				temp->index, stop->index);
+	/* Search for the value in the range */
+	while (temp != stop && temp->n < value)
+	{
+		printf("Value checked at index [%lu] = [%i]\n",
+				temp->index, temp->n);
+		temp = temp->next;
+	}
+	printf("Value checked at index [%lu] = [%i]\n",
+			temp->index, temp->n);
+	/* Check if the value is found and return the appropriate pointer */
+	if (temp == stop)
+		return (NULL);
+	return (temp);
 }
